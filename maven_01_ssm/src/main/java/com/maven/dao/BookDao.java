391b7d036202095fbb7c132f88d9510281702695
青衿 <2401492703@qq.com>
2025-02-06 20:16:34 +0800

    maven项目提交完毕

---
+++ maven_01_ssm/src/main/java/com/maven/dao/BookDao.java
@@ -0,0 +1,28 @@
+package com.maven.dao;
+
+
+import com.maven.entity.Book;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+
+public interface BookDao {
+
+    @Insert("insert into ssm_table (type,name,description) values(#{type},#{name},#{description})")
+    public int save(Book book);
+
+    @Update("update ssm_table set type = #{type}, name = #{name}, description = #{description} where id = #{id}")
+    public int update(Book book);
+
+    @Delete("delete from ssm_table where id = #{id}")
+    public int delete(Integer id);
+
+    @Select("select * from ssm_table where id = #{id}")
+    public Book getById(Integer id);
+
+    @Select("select * from ssm_table")
+    public List<Book> getAll();
+
+
+}
---
+++ maven_01_ssm/src/main/java/com/maven/service/impl/BookServiceImpl.java
@@ -0,0 +1,43 @@
+package com.maven.service.impl;
+
+import com.maven.dao.BookDao;
+import com.maven.entity.Book;
+import com.maven.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+
+public class BookServiceImpl implements BookService {
+
+    @Autowired
+    private BookDao bookDao;  //这里是mybatis的自动代理 没有交给sprint管理 所以说报错
+
+    public boolean save(Book book) {
+        //判断影响行数是否大于0
+       return bookDao.save(book)>0;
+
+    }
+
+    public boolean update(Book book) {
+        return bookDao.update(book)>0;
+
+    }
+
+    public boolean delete(Integer id) {
+       return bookDao.delete(id)>0;
+
+    }
+
+    public Book getById(Integer id) {
+        return bookDao.getById(id);
+    }
+
+    public List<Book> getAll() {
+        return bookDao.getAll();
+    }
+
+
+}
